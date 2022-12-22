43309148a60d85dbd642894f013303dd415d0d0c
Pratik Tharu <077BCT060.pratik@pcampus.edu.np>
2022-12-22 18:20:25 +0545

    To check the paired parenthesis in Mathematical Expresssion

---
+++ Stack/check_paired_parenthesis.cpp
@@ -0,0 +1,37 @@
+//To check the Paired Parenthesis in Mathematical Expression
+
+#include<iostream>
+#include<string.h>
+#include<stack>
+using namespace std;
+
+string evaluate(string exp){
+
+    stack<char> s;
+    for(int i=0; i<exp.size(); i++)
+    {
+
+        if(exp[i]=='('){
+            s.push('(');
+        }
+        if(exp[i]==')'){
+            s.pop();
+        }
+    }
+    if(s.empty()){
+        cout<<"parenthesis are matched";
+
+    }
+    else{
+        cout<<"parenthesis aer not matched";
+    }
+}
+
+int main(){
+
+    evaluate("((A+B)*(C+D)");
+    return 0;
+    
+
+}
+
