d878f921e885f7dbdf6e09408cd045df3da03452
Pratik Tharu <077BCT060.pratik@pcampus.edu.np>
2022-12-22 09:05:56 +0545

    Enqueue and dequeue operation circular Queue

---
+++ Queue/enque_deque_in_circularqueue.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#define SIZE 100
+#define GGMU 0
+using namespace std; 
+template<class T>
+class CQueue {
+private:
+T items[SIZE];
+int front, end; 
+public:
+CQueue() {
+front = end = 0; 
+}
+bool isEmpty() {
+return (front == end); 
+}
+void enqueue(T item) { 
+end % (SIZE - 1); 
+items[end++] = item;
+}
+T dequeue() {
+if (isEmpty()) throw "Stack Underflow"; 
+front %= SIZE;
+return items[front++];
+} 
+};
+int main() {
+CQueue<int> q; 
+try {
+for (int i = 0; i < 10; i++) 
+q.enqueue(i);
+while (!q.isEmpty())
+cout << q.dequeue();
+}
+catch (const char* err) {
+cout << err; 
+}
+return GGMU; 
+}
