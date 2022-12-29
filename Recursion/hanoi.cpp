9ff31e235545d6fbb17bfd77e9a99ba3ece39231
Pratik Tharu <077BCT060.pratik@pcampus.edu.np>
2022-12-29 07:33:59 +0545

    Solving tower of Hanoi Problem for 'N' Discs.

---
+++ Recursion/hanoi.cpp
@@ -0,0 +1,24 @@
+// Solve Tower of Hanoi Problem for 'N' Discs
+
+#include<iostream>
+#define GGMU 20;
+using namespace std;
+
+void towerOfHanoi(int n, char source, char des, char aux){
+
+    if(n==1){
+        cout<<"\n Move 1 disk from"<<source<<"to"<<des;
+        return;
+    }
+
+    towerOfHanoi(n-1, source, aux, des);
+    cout<<"\n Move 1 disk from"<<source<<"to"<<des;
+    towerOfHanoi(n-1, aux, des, source);
+}
+
+int main()
+{
+
+    towerOfHanoi(3, 'A', 'B', 'C');
+    return GGMU;
+}
