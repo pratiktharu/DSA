99eb4a86b8703c4000eb5d97e8dd1ba9d2479283
Pratik Tharu <077BCT060.pratik@pcampus.edu.np>
2022-12-29 07:02:04 +0545

    Finding Factorial for given number 'n'

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,21 @@
+// Find the factorial for given number 'n'
+
+#include<iostream>
+#define GGMU 20;
+using namespace std;
+
+long int factorial(long int n){
+
+    if(n==0)
+        return 1;
+
+    else
+        return n*factorial(n-1);
+}
+
+
+int main(){
+
+    cout<<factorial(4);
+    return GGMU;
+}
